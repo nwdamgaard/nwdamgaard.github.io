ac212f30ef0d5c9a681f526f150e6a693f4afdea
Nathan <nwdamgaard@gmail.com>
2018-10-20 18:21:40 -0700

    made other entries fade out on click

--- script.js
+++ script.js
@@ -5,17 +5,54 @@ var entry3 = document.getElementById("entry-three");
 var entry4 = document.getElementById("entry-four");
 
 function clickEntry1 () {
-    
+    playAnimation(entry2);
+    playAnimation(entry3);
+    playAnimation(entry4);
+
+    setTimeout(function() {
+        entry2.style.display = "none";
+        entry3.style.display = "none";
+        entry4.style.display = "none";
+    }, 800);
 }
 
 function clickEntry2 () {
+    playAnimation(entry1);
+    playAnimation(entry3);
+    playAnimation(entry4);
 
+    setTimeout(function() {
+        entry1.style.display = "none";
+        entry3.style.display = "none";
+        entry4.style.display = "none";
+    }, 800);
 }
 
 function clickEntry3 () {
+    playAnimation(entry1);
+    playAnimation(entry2);
+    playAnimation(entry4);
 
+    setTimeout(function() {
+        entry1.style.display = "none";
+        entry2.style.display = "none";
+        entry4.style.display = "none";
+    }, 800);
 }
 
 function clickEntry4 () {
+    playAnimation(entry2);
+    playAnimation(entry3);
+    playAnimation(entry1);
 
+    setTimeout(function() {
+        entry2.style.display = "none";
+        entry3.style.display = "none";
+        entry1.style.display = "none";
+    }, 800);
+}
+
+function playAnimation(entry) {
+    entry.style.webkitAnimationPlayState = "running";
+    entry.style.animationPlayState = "running";
 }
