9d3981ca68a8396cdb206aa15a9bca752b6b7910
Nathan <nwdamgaard@gmail.com>
2018-10-20 17:52:31 -0700

    added entry variables in script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 
+var entry1 = document.getElementById("entry-one");
+var entry2 = document.getElementById("entry-two");
+var entry3 = document.getElementById("entry-three");
+var entry4 = document.getElementById("entry-four");
+
 function clickEntry1 () {
 
 }
@@ -12,5 +17,5 @@ function clickEntry3 () {
 }
 
 function clickEntry4 () {
-    
+
 }
